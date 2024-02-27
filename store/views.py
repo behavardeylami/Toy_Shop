2efcb1eab4d973c8e0a038e05dd459caabd2124e
Behavar Deylami <behavardeylami@gmail.com>
2024-02-27 20:27:58 +0330

    pep8

--- store/views.py
+++ store/views.py
@@ -37,25 +37,13 @@ def product_detail(request, product_id):
         {'product': product, 'approved_comments': approved_comments}
     )
 
-# def post_detail(request, post_id):
-#     post = get_object_or_404(Post, id=post_id)
-#     # Filter and serialize only approved comments
-#     approved_comments = post.comments.filter(approved=True)
-#     serializer = PostSerializer(post)
-#     return render(request, 'blog/post_detail.html', {'post': post, 'approved_comments': approved_comments})
-
 
 def comment_view(request, product_id):
     if request.method == "Product":
-        # Retrieve data from the form
-        # author = request.user.id
-        # comment_text = request.POST.get("comment", "")
 
         author = request.POST.get("author", "")
         comment_text = request.POST.get("comment", "")
-        # Perform any necessary validation here
 
-        # Create a new Comment instance and save it to the database
         comment = Comment(author_id=author, text=comment_text, product_id=product_id, approved=False)
         comment.save()
 
@@ -64,37 +52,10 @@ def comment_view(request, product_id):
         )
         comment.save()
 
-        # Redirect to a success page or the same page with a success message
         return redirect(reverse("product_detail", args=[product_id]))
     else:
         return redirect(reverse("product_detail", args=[product_id]))
 
-        
-    #     comment = Comment(
-    #         author=author, body=comment_text, post=Post.objects.get(id=post_id)
-    #     )
-    #     comment.save()
-
-    #     # Redirect to a success page or the same page with a success message
-    #     return redirect(reverse("post_detail", args=[post_id]))
-    # else:
-    #     return redirect(reverse("post_detail", args=[post_id]))
-# def comment_view(request, product_id):
-#     if request.method == "POST":
-#         # Retrieve data from the form
-#         author = request.user.id  # Assuming you want to associate the comment with the logged-in user
-#         comment_text = request.POST.get("text", "")
-
-#         # Perform any necessary validation here
-
-#         # Create a new Comment instance and save it to the database
-#         comment = Comment(author_id=author, text=comment_text, product_id=product_id, approved=False)
-#         comment.save()
-
-#         # Redirect to a success page or the same page with a success message
-#         return redirect(reverse("product_detail", args=[product_id]))
-#     else:
-#         return redirect(reverse("product_detail", args=[product_id]))
 
 # API Views
 @api_view(['GET'])
